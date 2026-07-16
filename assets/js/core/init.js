fdb18bfe03ef254ed5f11a589b62dfdff29c2ccd
Harrickdiagnosticlab <hattrickdiagnosticlab@gmail.com>
2026-07-17 02:15:20 +0530

    Delete assets directory

--- assets/js/core/init.js
+++
@@ -1,7 +0,0 @@
-  // ---------- Init ----------
-  syncInvInvoiceDate = wireDateDisplay('invInvoiceDate', 'invInvoiceDateDisplay');
-  syncAcctFilterDate = wireDateDisplay('acctFilterDate', 'acctFilterDateDisplay');
-  wireDateDisplay('ledgerStartDate', 'ledgerStartDateDisplay');
-  wireDateDisplay('ledgerEndDate', 'ledgerEndDateDisplay');
-  syncExpDate = wireDateDisplay('expDate', 'expDateDisplay');
-  showScreen('auth');
--- assets/js/core/supabase-client.js
+++
@@ -1,4 +0,0 @@
-  const SUPABASE_URL = "https://ghpyktztztxrwombheah.supabase.co";
-  const SUPABASE_ANON_KEY = "sb_publishable_DcV-CK4A8aq_xxYOO7LMEA_Djr3Tz90";
-  const sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
